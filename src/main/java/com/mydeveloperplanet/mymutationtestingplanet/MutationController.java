85a953b924f8d514bd93e29d184028451445f73a
mydeveloperplanet <gunter@mydeveloperplanet.com>
2020-02-29 14:49:42 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/mymutationtestingplanet/MutationController.java
@@ -0,0 +1,22 @@
+package com.mydeveloperplanet.mymutationtestingplanet;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MutationController {
+
+    @GetMapping("/compareToFifty/{value}")
+    public String compareToFifty(@PathVariable int value) {
+        String message = "Could not determine comparison";
+        if (value > 50) {
+            message = "Greater than 50";
+        } else {
+            message = "Smaller than or equal to 50";
+        }
+
+        return message;
+    }
+
+}
---
+++ src/main/java/com/mydeveloperplanet/mymutationtestingplanet/MyMutationTestingPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.mymutationtestingplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMutationTestingPlanetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMutationTestingPlanetApplication.class, args);
+    }
+
+}
